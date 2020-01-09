6ac0f0a8776e606bee755f3e727c31edb46f6433
dhruvpatel19 <53876897+dhruvpatel19@users.noreply.github.com>
2020-01-08 23:21:22 -0500

    Update n-queens.py

--- n-queens.py
+++ n-queens.py
@@ -13,5 +13,6 @@ def solve(n, i, a, b, c):
 
 #To obtain different results each time, I simply defined a random index value, and issued a print command to the output of the generator function at that index
 index = random.randint(1, 10000) #39,029,188,884 = total number of solutions to the n-queens problem at n=20
+
 #index set to the range 10000 due to excessive runtime for generator to iterate to the millionth index, for example
 print(next(itertools.islice(solve(20, 0, [], [], []), index, None)))
